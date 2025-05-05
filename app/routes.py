79995ec6c417faddc2e0ef7b6d6266bbbd2728d6
apachete <ddaassmg@gmail.com>
2025-05-05 20:25:13 +0200

    login básico funcionando
    movido lanzador de la app al fichero routes.py

--- app/routes.py
+++ app/routes.py
@@ -1,10 +1,54 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for, session
+from dotenv import load_dotenv
+import os
+
+
 app = Flask(__name__)
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+
+# Cargar variables de entorno necesarias
+load_dotenv()
+app.secret_key = os.getenv('SECRET_KEY')
+USER = os.getenv('USERAPP')
+PASS = os.getenv('PASS')
 
 # INDEX
 @app.route('/')
 def index():
-    return render_template('index.html')
+    if 'usuario' in session:
+        return f"Hola, {session['usuario']}! <a href='/logout'>Cerrar sesión</a>"
+    return redirect(url_for('login'))
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        usuario = request.form['usuario']
+        contrasena = request.form['contrasena']
+        print(f"usuario: '{usuario}', contrasena: '{contrasena}'")
+        print(f"USUARIO: '{USER}', CONTRASENA: '{PASS}'")
+
+        if usuario == USER and contrasena == PASS:
+            session['usuario'] = usuario
+            return redirect(url_for('index'))
+        else:
+            return "Credenciales inválidas. <a href='/login'>Intentar de nuevo</a>"
+    return '''
+        <form method="post">
+            Usuario: <input type="text" name="usuario"><br>
+            Contraseña: <input type="password" name="contrasena"><br>
+            <input type="submit" value="Iniciar sesión">
+        </form>
+    '''
+
+@app.route('/logout')
+def logout():
+    session.pop('usuario', None)
+    return redirect(url_for('login'))
+
 
 
 @app.route('/exercise1')
