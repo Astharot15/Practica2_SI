40eeab63f604e2d761f6d627cdb39fae8790844d
Astharot15 <familyzr2014@gmail.com>
2025-05-07 17:15:00 +0200

    Fixed

--- app/app.py
+++ app/app.py
@@ -1,6 +1,20 @@
 from flask import Flask
+from flask import Flask, render_template
+import os
 
-app = Flask(__name__)
+template_dir = os.path.abspath('../templates')
+static_dir = os.path.abspath('../static')
+
+app = Flask(__name__, template_folder=template_dir, static_folder=static_dir)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/exercise1')
+def exercise1():
+    return render_template('exercise1.html')
 
 if __name__ == '__main__':
     app.run()
